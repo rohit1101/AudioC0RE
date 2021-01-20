51cc25b4cb16046746522e8f605feed6de84cdfd
rohit1101 <srohit1101@gmail.com>
2021-01-20 17:16:04 +0530

    Add styles

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -2,10 +2,17 @@ import React from "react"
 import { Link } from "gatsby"
 function Container({ children }) {
   return (
-    <div>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/collection">Collections</Link>
+    <div style={{ maxWidth: "90%", margin: "0 auto", background: "slategray" }}>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <div>
+          <Link to="/">Home</Link>
+        </div>
+        <div>
+          <Link to="/about">About</Link>{" "}
+          <Link to="/collection">Collections</Link>
+        </div>
+      </div>
+
       {children}
     </div>
   )

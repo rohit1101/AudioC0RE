99ff02e58274ebd4371065b736f08351fe1871b6
rohit1101 <srohit1101@gmail.com>
2021-01-20 16:36:08 +0530

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  siteMetadata: {
+    title: "AudioC0Re",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: "oDOgV3Kcpk-0wAR3AhMmSiKB9juDNJcQoLcOk2FE_H4",
+        spaceId: "1lrf2559kdgl",
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};

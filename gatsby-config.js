54a31750edffabc585de984b86f7ce4f48752591
rohit1101 <srohit1101@gmail.com>
2021-01-25 19:55:48 +0530

    using strapi

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,13 +4,6 @@ module.exports = {
     siteUrl: "https://audioc0re.io",
   },
   plugins: [
-    {
-      resolve: "gatsby-source-contentful",
-      options: {
-        accessToken: "oDOgV3Kcpk-0wAR3AhMmSiKB9juDNJcQoLcOk2FE_H4",
-        spaceId: "1lrf2559kdgl",
-      },
-    },
     "gatsby-plugin-sharp",
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
--- src/pages/collections/novacorp.js
+++ src/pages/collections/novacorp.js
@@ -1,6 +1,5 @@
 import React from "react"
 import Container from "../../components/Container"
-import { Link } from "gatsby"
 
 const NovaCorp = () => (
   <Container>

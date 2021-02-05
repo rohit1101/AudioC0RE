3414fe6be7bc27a8662772d5aba6dd01ec5a2363
rohit1101 <srohit1101@gmail.com>
2021-02-05 15:05:59 +0530

    File system route bug

--- src/pages/collections.js
+++ src/pages/collections.js
@@ -1,15 +1,41 @@
 import React from "react"
 import Container from "../components/Container"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
-// const query = graphql``
+export const cityData = graphql`
+  query {
+    allContentfulCity {
+      edges {
+        node {
+          id
+          description
+          createdAt(fromNow: true)
+          name
+          node_locale
+        }
+      }
+    }
+  }
+`
 
-const Collections = () => (
-  <Container>
-    <Link to="/collections/asgard">Asgard</Link>{" "}
-    <Link to="/collections/novacorp">Nova Corp</Link>{" "}
-    <Link to="/collections/vormir">Vormir</Link>
-  </Container>
-)
+const Collections = (cityData) => {
+  const { data } = cityData
+  console.log(data)
+  return (
+    <div>
+      <Container>
+        <Link to="/collections/asgard">Asgard</Link>{" "}
+        <Link to="/collections/novacorp">Nova Corp</Link>{" "}
+        <Link to="/collections/vormir">Vormir</Link>
+      </Container>
+
+      {/* <div>
+        {data.allContentfulCity.edges.map((city) => {
+          return <div key={city.node.id}>{city.node.name}</div>
+        })}
+      </div> */}
+    </div>
+  )
+}
 
 export default Collections
